36b60fc5a3c5f7289d8b361e750e26eca3497fd2
t1kwelli <dimma1374@gmail.com>
2020-08-05 22:59:12 +0500

    homework from step 2.4

---
+++ lesson_2.4.py
@@ -0,0 +1,39 @@
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium import webdriver
+import math
+
+
+def calc(x):
+  return str(math.log(abs(12*math.sin(int(x)))))
+
+link = "http://suninjuly.github.io/explicit_wait2.html"
+
+try:
+    browser = webdriver.Chrome()
+    browser.get(link)
+
+    price = WebDriverWait(browser, 12).until(
+        EC.text_to_be_present_in_element((By.ID, "price"), "$100")
+    )
+    button = browser.find_element_by_class_name("btn")
+    button.click()
+
+    x_element = browser.find_element_by_id("input_value")
+    x = x_element.text
+    y = calc(x)
+
+    input = browser.find_element_by_id("answer")
+    input.send_keys(y)
+
+    button = browser.find_element_by_id("solve")
+    button.click()
+
+finally:
+    # успеваем скопировать код за 30 секунд
+    time.sleep(10)
+    # закрываем браузер после всех манипуляций
+    browser.quit()
+
+# не забываем оставить пустую строку в конце файла
